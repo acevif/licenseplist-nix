85fd1b66d57f6465545e6ea829e75f5c51e75317
acevif <acevif@gmail.com>
2026-03-25 05:51:57 +0900

    Expand published LicensePlist versions

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Versioned [LicensePlist](https://github.com/mono0926/LicensePlist) binary packag
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
-          licenseplist-nix.packages.${system}.licenseplist_3_27_1
+          licenseplist-nix.packages.${system}.licenseplist_3_27_2
         ];
       };
     };
@@ -37,7 +37,7 @@ license-plist --version
 ### Direct run
 
 ```bash
-nix run github:acevif/licenseplist-nix#licenseplist_3_27_1 -- --version
+nix run github:acevif/licenseplist-nix#licenseplist_3_27_2 -- --version
 ```
 
 You can also use the rolling aliases:
@@ -50,10 +50,17 @@ nix run github:acevif/licenseplist-nix#licenseplist -- --version
 
 ### Versions
 
+- `licenseplist_3_27_2` - LicensePlist 3.27.2 (`x86_64-darwin`, `aarch64-darwin`)
 - `licenseplist_3_27_1` - LicensePlist 3.27.1 (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_27_0` - LicensePlist 3.27.0 (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_26_0` - LicensePlist 3.26.0 (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_25_1` - LicensePlist 3.25.1 (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_25_0` - LicensePlist 3.25.0 (`x86_64-darwin`, `aarch64-darwin`)
 
 ### Aliases
 
 - `licenseplist_3_27` - 3.27.x latest (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_26` - 3.26.x latest (`x86_64-darwin`, `aarch64-darwin`)
+- `licenseplist_3_25` - 3.25.x latest (`x86_64-darwin`, `aarch64-darwin`)
 - `licenseplist` - latest (`x86_64-darwin`, `aarch64-darwin`)
 - `default` - latest (`x86_64-darwin`, `aarch64-darwin`)
--- flake.nix
+++ flake.nix
@@ -18,23 +18,33 @@
         { pkgs, system, ... }:
         let
           mkLicenseplist = args: pkgs.callPackage ./packages/mkLicenseplist.nix args;
-          versioned = {
-            licenseplist_3_27_1 = mkLicenseplist {
-              versionId = "3_27_1";
-              platforms = [
-                "x86_64-darwin"
-                "aarch64-darwin"
-              ];
+          darwinPlatforms = [
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ];
+          mkVersion = versionId:
+            mkLicenseplist {
+              inherit versionId;
+              platforms = darwinPlatforms;
             };
+          versioned = {
+            licenseplist_3_25_0 = mkVersion "3_25_0";
+            licenseplist_3_25_1 = mkVersion "3_25_1";
+            licenseplist_3_26_0 = mkVersion "3_26_0";
+            licenseplist_3_27_0 = mkVersion "3_27_0";
+            licenseplist_3_27_1 = mkVersion "3_27_1";
+            licenseplist_3_27_2 = mkVersion "3_27_2";
           };
         in
         {
           packages = pkgs.lib.filterAttrs (_: pkg: builtins.elem system pkg.meta.platforms) (
             versioned
             // {
-              licenseplist_3_27 = versioned.licenseplist_3_27_1;
-              licenseplist = versioned.licenseplist_3_27_1;
-              default = versioned.licenseplist_3_27_1;
+              licenseplist_3_25 = versioned.licenseplist_3_25_1;
+              licenseplist_3_26 = versioned.licenseplist_3_26_0;
+              licenseplist_3_27 = versioned.licenseplist_3_27_2;
+              licenseplist = versioned.licenseplist_3_27_2;
+              default = versioned.licenseplist_3_27_2;
             }
           );
 
